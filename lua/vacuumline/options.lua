a2ab5a6ad3a9486e1ae708f755a87c790f04b14a
konapun <bremen.braun@gmail.com>
2021-07-28 23:09:12 -0500

    work on merging options

--- lua/vacuumline/options.lua
+++ lua/vacuumline/options.lua
@@ -1,42 +1,101 @@
 local M = {}
 
-local palette = {
-  background = '#282828',
-  foreground = '#ebdbb2',
-  accent     = '#b8bb26',
-  black      = '#282828',
-  red        = '#cc241d',
-  green      = '#98971a',
-  yellow     = '#d79921',
-  blue       = '#458588',
-  purple     = '#b16286',
-  cyan       = '#689d6a',
-  white      = '#a89984',
-  bright_black = '#928374',
-  bright_red   = '#fb4934',
-  bright_green = '#b8bb26',
-  bright_yellow = '#fabd2f',
-  bright_blue   = '#83a598',
-  bright_purple = '#d3869b',
-  bright_cyan   = '#8ec07c',
-  bright_white  = '#ebdbb2'
-}
-
 -- Perform a one dimensional merge over two tables
 local function merge(t1, t2)
   local merged = {}
   for k,v in pairs(t1) do
     merged[k] = v
   end
-  for k,v in pairs(t2) do
-    merged[k] = v
+
+  if t2 then
+    for k,v in pairs(t2) do
+      merged[k] = v
+    end
   end
 
   return merged
 end
 
+local default_options = {
+  separator = {
+    segment = {
+      left = '',
+      right = ''
+    },
+    section = {
+      left = '',
+      right = ''
+    }
+  },
+  color = {
+    foreground = {even = '#282828', odd = '#282828'},
+    background = {even = '#b16286', odd = '#98971a'},
+  },
+  segment = {
+    mode = {
+      enabled = true,
+      map = { -- TODO
+        n = {text = ' ', background = '#b16286'}, -- NORMAL
+        i = {text = ' ', background = '#98971a'}, -- INSERT
+        c = {text = ' ', background = '#458588'}, -- COMMAND
+        v = {text = ' ', background = '#d79921'}, -- VISUAL
+        V = {text = ' ', background = '#fabd2f'}, -- VISUAL LINE
+        t = {text = ' ', background = '#d3869b'}, -- TERMINAL
+        s = {text = 's', background = '#fb4934'},
+        S = {text = 'S', background = '#b8bb26'},
+        R = {text = 'R', background = '#b16286'},
+        r = {text = 'r', background = '#b16286'},
+        ce = {text = 'ce', background = '#b16286'},
+        cv = {text = 'cv', background = '#b16286'},
+        ic = {text = 'ic', background = '#8ec07c'},
+        no = {text = 'no', background = '#fabd2f'},
+        rm = {text = 'rm', background = '#b16286'},
+        Rv = {text = 'Rv', background = '#b16286'},
+        ['!'] = {text = '!', background = '#b16286'},
+        [''] = {text = '^S', background = '#83a598'},
+        ['^V'] = {text = ' ', background = '#680d6a'}, -- VISUAL BLOCK
+        ['r?'] = {text = 'r?', background = '#b16286'},
+      }
+    },
+    file = {
+      enabled = true,
+    },
+    vcs = {
+      enabled = true,
+    },
+    scroll = {
+      enabled = true,
+      foreground = '',
+      background = '',
+      accent = '',
+    },
+    lines = {
+      enabled = true,
+    },
+    diagnostics = {
+      enabled = true,
+      errors = {
+        foreground = '',
+        background = ''
+      },
+      warnings = {
+        foreground = '',
+        background = ''
+      }
+    },
+    search = {
+      enabled = true,
+    },
+    lsp = {
+      enabled = true,
+    }
+  }
+}
+
 -- Configure and format vacuumline options based on user input
 function M.format(opts, segments)
+  opts = opts ~= {separator = {}, color = {}, segment = {}}
+
   -- Set up defaults for each config section
   local separator_config = merge({
     segment = {
@@ -56,40 +115,36 @@ function M.format(opts, segments)
 
   local segment_defaults = {
     mode = merge({
-      enabled = true,
-      background = palette.purple,
-      foreground = palette.background,
-      separator = separator_left,
       map = {
-        n      = {text = ' ', background = palette.purple}, -- NORMAL
-        i      = {text = ' ', background = palette.green}, -- INSERT
-        c      = {text = ' ', background = palette.blue}, -- COMMAND
-        v      = {text = ' ', background = palette.yellow}, -- VISUAL
-        V      = {text = ' ', background = palette.bright_yellow}, -- VISUAL LINE
-        ['^V'] = {text = ' ', background = palette.cyan}, -- VISUAL BLOCK
-        t      = {text = ' ', background = palette.bright_purple}, -- TERMINAL
+        n      = {text = ' ', background = '#b16286'}, -- NORMAL
+        i      = {text = ' ', background = '#98971a'}, -- INSERT
+        c      = {text = ' ', background = '#458588'}, -- COMMAND
+        v      = {text = ' ', background = '#d79921'}, -- VISUAL
+        V      = {text = ' ', background = '#fabd2f'}, -- VISUAL LINE
+        ['^V'] = {text = ' ', background = '#680d6a'}, -- VISUAL BLOCK
+        t      = {text = ' ', background = '#d3869b'}, -- TERMINAL
         -- TODO
-        no     = {text = 'no', background = palette.bright_yellow},
-        s      = {text = 's', background = palette.bright_red},
-        S      = {text = 'S', background = palette.bright_green},
-        [''] = {text = '^S', background = palette.bright_blue},
-        ic     = {text = 'ic', background = palette.bright_cyan},
-        R      = {text = 'R', background = palette.purple},
-        Rv     = {text = 'Rv', background = palette.purple},
-        cv     = {text = 'cv', background = palette.purple},
-        ce     = {text = 'ce', background = palette.purple},
-        r      = {text = 'r', background = palette.purple},
-        rm     = {text = 'rm', background = palette.purple},
-        ['r?'] = {text = 'r?', background = palette.purple},
-        ['!']  = {text = '!', background = palette.purple}
+        no     = {text = 'no', background = '#fabd2f'},
+        s      = {text = 's', background = '#fb4934'},
+        S      = {text = 'S', background = '#b8bb26'},
+        [''] = {text = '^S', background = '#83a598'},
+        ic     = {text = 'ic', background = '#8ec07c'},
+        R      = {text = 'R', background = '#b16286'},
+        Rv     = {text = 'Rv', background = '#b16286'},
+        cv     = {text = 'cv', background = '#b16286'},
+        ce     = {text = 'ce', background = '#b16286'},
+        r      = {text = 'r', background = '#b16286'},
+        rm     = {text = 'rm', background = '#b16286'},
+        ['r?'] = {text = 'r?', background = '#b16286'},
+        ['!']  = {text = '!', background = '#b16286'}
       }
     }, opts.segment.mode),
     file = merge({}, opts.segment.file),
     vcs = merge({}, opts.segment.vcs),
     scroll = merge({
-      background = palette.blue,
-      foreground = palette.background,
-      accent = palette.yellow,
+      background = '#458588',
+      foreground = '#282828',
+      accent = '#d79921',
     }, opts.segment.scroll),
     lines = merge({}, opts.segment.lines),
     diagnostics = merge({
