fbc35144a232f134a020b67910660a1907708225
konapun <bremen.braun@gmail.com>
2021-08-02 21:25:02 -0500

    Start working on terminal line activation

--- lua/vacuumline.lua
+++ lua/vacuumline.lua
@@ -1,12 +1,15 @@
 local gl = require('galaxyline')
+local event_processor = require('galaxyline.event-processor')
 local builder = require('vacuumline.builder')
-
-local M = {}
+local vim = vim
 
 gl.short_line_list = {'LuaTree', 'vista', 'dbui'}
 
+local M = {}
+
 function M.setup(opts)
-  return builder.build(gl, opts)
+  builder.build(gl, opts)
+  event_processor.activate()
 end
 
 return M
---
+++ lua/vacuumline/event-processor.lua
@@ -0,0 +1,21 @@
+local vim = vim
+
+--[[
+Mode utilities that enable vacuumline to have custom displays for terminal mode, etc
+--]]
+
+local M = {}
+
+-- Register event handlers
+function M.activate()
+  local api = vim.api
+
+  api.nvim_command("autocmd BufEnter * :lua require('vacuumline.event-processor').handle_buf_enter()")
+end
+
+function M.handle_buf_enter()
+  -- TODO check the current buffer type so the vacuumline can be updated (editor, terminal, etc)
+  -- local buftype =
+end
+
+return M
