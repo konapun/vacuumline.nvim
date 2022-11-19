3511b9dd2b80f7c7de53c5a0412dfdb2956f9446
konapun <bremen.braun@gmail.com>
2022-11-19 16:49:56 -0600

    Copy more providers from galaxyline

--- lua/vacuumline/provider/lsp.lua
+++ lua/vacuumline/provider/lsp.lua
@@ -1,5 +1,23 @@
-local function lsp_client()
-  -- TODO
+--[[
+Code adapted from https://github.com/glepnir/galaxyline.nvim/blob/main/lua/galaxyline/provider_lsp.lua
+--]]
+local vim = vim
+
+local function lsp_client(msg)
+  msg = msg or 'No Active Lsp'
+  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return client.name
+    end
+  end
+  return msg
 end
 
 return {
